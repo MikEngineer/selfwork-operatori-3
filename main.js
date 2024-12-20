655ac33baf7bf9aae37587a39be542269701965c
Michelangelo Ruisi <mruisi96@gmail.com>
2024-12-20 11:20:28 +0100

    terminato selfwork operatori 3

---
+++ main.js
@@ -0,0 +1,16 @@
+let num1 = 8;
+let num2 = 2;
+
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2;
+
+console.log(somma, sottrazione, moltiplicazione, divisione, potenza);
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma (${somma}), sottrazione (${sottrazione}), moltiplicazione (${moltiplicazione}), divisione (${divisione}) e potenza (${potenza}).`);
+
+
+
+
